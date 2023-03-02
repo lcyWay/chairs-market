345e1176819017521160effdabe13e7fc2cd82b0
lcyWay <carepuw@gmail.com>
2023-03-02 20:10:20 +0300

    chore: log redis post & host

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -12,6 +12,8 @@ const app = express();
 
 const port = process.env.PORT || 9876;
 
+console.log(process.env.REDIS_SERVICE_PORT, process.env.REDIS_SERVICE_HOSTNAME);
+
 const redisClient = redis.createClient(process.env.REDIS_SERVICE_PORT, process.env.REDIS_SERVICE_HOSTNAME);
 redisClient.connect().catch(console.error);
 
